b5ef89d8ceceb17730347a98da9ed60351678fa2
Calvin Froedge <calvinfroedge@gmail.com>
2013-10-23 12:20:39 -0500

    v0.0.0

---
+++ legal.js
@@ -0,0 +1,51 @@
+angular.module('10digit.legal', ['ngResource', '10digit.utils'])
+
+.factory('LegalConfig', function(){
+    var config = {
+        testMode: false,
+        initialValues: {
+            legal: true
+        }
+    }
+    return config;
+})
+
+.controller('LegalCtrl',['$scope', '$resource', 'ModalInstanceService', 'LegalConfig', function($scope, $resource, Modal, Config){
+	if(Config.testing){
+		$scope.legal = Config.initialValues.legal;
+	}
+	if(!$scope['legal_']) $scope['legal_'] = {};
+	$scope.agreements = {tos: '', loa: '', privacy: ''};
+	var Resource = $resource('/api/legal');
+	var resource = Resource.get({}, function(){
+		$scope.agreements = resource;
+	});
+
+	$scope.agreement = function(title, content){
+		var modal = Modal.open({
+			scope: $scope,
+			resolve: {
+				title: function(){ return title; },
+				content: function(){ return $scope.agreements[content]; },
+				okText: function(){ return 'ACCEPT';}
+			}
+		});
+
+		modal.result.then(function(){
+			var key = function(){ return content + '_signed';}
+			$scope['legal_'][key()] = true;
+		});
+
+		return false;
+	}
+}])
+
+.directive('legalAgreement', function () {
+	return {
+		restrict:'E',
+		controller:'LegalCtrl',
+		replace: false,
+		templateUrl: 'template/10digit/legal.html'
+	};
+});
+
